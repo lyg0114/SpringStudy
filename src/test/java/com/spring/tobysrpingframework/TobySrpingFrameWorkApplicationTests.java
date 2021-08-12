4e39f9153d6c68f95e1dcdabe2383f4efec32bbd
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-08-13 07:11:26 +0900

    1.1.2 초난감 DAO, 1.1.3 main()을 이용한 DAO 테스트 코드

--- src/test/java/com/spring/tobysrpingframework/TobySrpingFrameWorkApplicationTests.java
+++ src/test/java/com/spring/tobysrpingframework/TobySrpingFrameWorkApplicationTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TobySrpingFrameWorkApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    public void test(){
+        System.out.println("########");
     }
 
 }
