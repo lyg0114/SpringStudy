50726082711be6c674f7d48df9c6c78a4ff2b627
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-08-15 18:08:50 +0900

    1.7.4 의존관계 주입의 응용
    	- DB 연결 카운트 기능 추가
    	- ConnectionMaker 인터페이스를 구현하여 , 디비연결 카운트 기능 추가

---
+++ src/main/java/com/spring/tobysrpingframework/user/dao/UserDaoConnectionCountingTest.java
@@ -0,0 +1,39 @@
+package com.spring.tobysrpingframework.user.dao;
+
+import com.spring.tobysrpingframework.user.domain.User;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+public class UserDaoConnectionCountingTest {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(CountingDaoFactory.class);
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user = new User();
+        user.setId("yglee");
+        user.setName("이초다");
+        user.setPassword("married");
+        dao.add(user);
+
+        User user2 = new User();
+        user2.setId("yglee2");
+        user2.setName("이초다2");
+        user2.setPassword("married2");
+        dao.add(user2);
+
+        User user3 = new User();
+        user3.setId("yglee3");
+        user3.setName("이초다3");
+        user3.setPassword("married3");
+        dao.add(user3);
+
+
+
+        CountingConnectionMaker ccm = context.getBean("connectionMaker", CountingConnectionMaker.class);
+        System.out.println("Connection counter : " + ccm.getCounter());
+
+    }
+}
