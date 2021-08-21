abb3a3863a6f70009e185fcc45818b008081fa1e
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-08-21 17:51:34 +0900

    1.8.3 DataSource 인터페이스로 변환

--- src/main/java/com/spring/tobysrpingframework/user/dao/DataFactory.java
+++ src/main/java/com/spring/tobysrpingframework/user/dao/DataFactory.java
@@ -1,7 +1,14 @@
 package com.spring.tobysrpingframework.user.dao;
 
+import com.sun.org.apache.bcel.internal.generic.SIPUSH;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @Configuration
 public class DataFactory {
@@ -10,13 +17,19 @@ public class DataFactory {
     public UserDao userDao() {
         //UserDao dao = new UserDao(connectionMaker());
         UserDao dao = new UserDao();
-        dao.setConnectionMaker(connectionMaker());
+        dao.setDataSource(dataSource());
         return dao;
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
-        ConnectionMaker connectionMaker = new DConnectionMaker();
-        return connectionMaker;
+    public DataSource dataSource(){
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://lizcalendal-mysql.canpeabsn7mi.ap-northeast-2.rds.amazonaws.com:3306/spring_study");
+        dataSource.setUsername("kylelovelizzy");
+        dataSource.setPassword("kylelovelizzy");
+
+        return dataSource;
+
     }
 }
--- src/main/java/com/spring/tobysrpingframework/user/dao/UserDao.java
+++ src/main/java/com/spring/tobysrpingframework/user/dao/UserDao.java
@@ -2,26 +2,22 @@ package com.spring.tobysrpingframework.user.dao;
 
 import com.spring.tobysrpingframework.user.domain.User;
 
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
 
-    private ConnectionMaker connectionMaker;
+    private DataSource dataSource;
 
     public UserDao(){
-
     }
 
-    public UserDao(ConnectionMaker connectionMaker) {  // UserDao는 connectionMaker의 Object에는 관심이 없다.
-        this.connectionMaker = connectionMaker;
-    }
-
-    public void setConnectionMaker(ConnectionMaker connectionMaker){
-        this.connectionMaker = connectionMaker;
+    public void setDataSource(DataSource dataSource){
+        this.dataSource = dataSource;
     }
 
     public void add(User user) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)");
@@ -37,7 +33,7 @@ public class UserDao {
 
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = connectionMaker.makeConnection();
+        Connection c = dataSource.getConnection();
 
         PreparedStatement ps = c
                 .prepareStatement("select * from users where id = ?");
