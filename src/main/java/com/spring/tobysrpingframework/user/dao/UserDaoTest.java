762a40f1122248bb8e4a9fc447eb12108a301b7e
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-08-22 10:20:05 +0900

    2.3.3 포괄적인 테스트 	- 세밀한 테스트 작업

--- src/main/java/com/spring/tobysrpingframework/user/dao/UserDaoTest.java
+++ src/main/java/com/spring/tobysrpingframework/user/dao/UserDaoTest.java
@@ -19,20 +19,51 @@ public class UserDaoTest {
 
         //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DataFactory.class);
         GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
-
-
         UserDao dao = context.getBean("userDao", UserDao.class);
 
-        User user = new User();
-        user.setId("yglee");
-        user.setName("이초다");
-        user.setPassword("springno1");
+        User user1 = new User("kyle1","이초다1","password1");
+        User user2 = new User("kyle2","이초다2","password2");
 
-        dao.add(user);
-        User user2 = dao.get(user.getId());
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
 
+        dao.add(user1);
+        dao.add(user2);
+
+        assertThat(dao.getCount(), is(2));
+
+        User userGet1 = dao.get(user1.getId());
+        assertThat(userGet1.getName(), is(user1.getName()));
+        assertThat(userGet1.getPassword(), is(user1.getPassword()));
+
+        User userGet2 = dao.get(user2.getId());
+        assertThat(userGet2.getName(), is(user2.getName()));
+        assertThat(userGet2.getPassword(), is(user2.getPassword()));
+
+
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        //AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DataFactory.class);
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+
+        User user1 = new User("kyle1","이초다1","password1");
+        User user2 = new User("kyle2","이초다2","password2");
+        User user3 = new User("kyle3","이초다3","password3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
     }
 
     public static void main(String[] args) {
--- src/main/java/com/spring/tobysrpingframework/user/domain/User.java
+++ src/main/java/com/spring/tobysrpingframework/user/domain/User.java
@@ -1,8 +1,15 @@
 package com.spring.tobysrpingframework.user.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.beans.ConstructorProperties;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private String id;
