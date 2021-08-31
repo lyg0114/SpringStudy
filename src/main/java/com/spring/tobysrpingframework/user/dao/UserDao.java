335d27810d0494d053c75933904d3f9821279abf
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-09-01 06:52:35 +0900

    3.6.3 queryForObject()

--- src/main/java/com/spring/tobysrpingframework/user/dao/UserDao.java
+++ src/main/java/com/spring/tobysrpingframework/user/dao/UserDao.java
@@ -7,6 +7,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 import java.sql.*;
@@ -35,29 +36,19 @@ public class UserDao {
 
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Connection c = dataSource.getConnection();
-
-        PreparedStatement ps = c
-                .prepareStatement("select * from users where id = ?");
-        ps.setString(1, id);
-
-        ResultSet rs = ps.executeQuery();
-
-        User user = null;
-        if (rs.next()) {
-            user = new User();
-            user.setId(rs.getString("id"));
-            user.setName(rs.getString("name"));
-            user.setPassword(rs.getString("password"));
-        }
-
-        rs.close();
-        ps.close();
-        c.close();
-
-        if (user == null) throw new EmptyResultDataAccessException(1);
-
-        return user;
+       return this.jdbcTemplate.queryForObject(
+               "select * from users where id = ?",
+               new Object[]{id},
+               new RowMapper<User>() {
+                   @Override
+                   public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                       User user = new User();
+                       user.setId(rs.getString("id"));
+                       user.setName(rs.getString("name"));
+                       user.setPassword(rs.getString("password"));
+                       return user;
+                   }
+               });
     }
 
     public void deleteAll() throws SQLException {
@@ -86,8 +77,6 @@ public class UserDao {
                     }
                 }
         );
-
-
     }
 
 
