d35bd9c9c6822b748ab0888d73a7293e98dedee8
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-09-20 08:44:26 +0900

    5.1.3. UserService.upgradeLevels() 초기 모델

---
+++ src/main/java/com/spring/tobysrpingframework/user/service/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.spring.tobysrpingframework.user.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/test-applicationContext.xml")
+public class UserServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Test
+    public void bean(){
+        assertThat(this.userService, is(notNullValue()));
+    }
+
+
+}
