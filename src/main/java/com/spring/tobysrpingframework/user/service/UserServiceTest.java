bde54fb661167feab213886fdf3dafb0cc338751
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-09-20 08:53:09 +0900

    5.1.3. UserService.upgradeLevels() 테스트

--- src/main/java/com/spring/tobysrpingframework/user/service/UserServiceTest.java
+++ src/main/java/com/spring/tobysrpingframework/user/service/UserServiceTest.java
@@ -1,10 +1,17 @@
 package com.spring.tobysrpingframework.user.service;
 
+import com.spring.tobysrpingframework.user.dao.UserDao;
+import com.spring.tobysrpingframework.user.domain.Level;
+import com.spring.tobysrpingframework.user.domain.User;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -17,9 +24,26 @@ public class UserServiceTest {
     @Autowired
     UserService userService;
 
+    @Autowired
+    UserDao userDao;
+
+    List<User> users;
+
     @Test
     public void bean(){
         assertThat(this.userService, is(notNullValue()));
+    }
+
+    @Before
+    public void setUp(){
+        users = Arrays.asList(
+                new User("bumjin", "박범진", "p1", Level.BASIC, 49, 0),
+                new User("joytouch", "강명성", "p2", Level.BASIC, 50, 0),
+                new User("erwins", "신승환", "p3", Level.SILVER, 60, 29),
+                new User("madnite1", "이상호", "p4", Level.SILVER, 60, 30),
+                new User("green", "오민규", "p5", Level.GOLD, 100,100)
+        );
+
     }
 
 
