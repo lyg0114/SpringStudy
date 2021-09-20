30144f22f9f12f2090485a47ba5084e4aa5b7458
yglee@ksmartpia.com <whdnseowkd@naver.com>
2021-09-20 09:29:58 +0900

    usergradeLevels() 리팩토릭

--- src/main/java/com/spring/tobysrpingframework/user/service/UserService.java
+++ src/main/java/com/spring/tobysrpingframework/user/service/UserService.java
@@ -14,6 +14,7 @@ public class UserService {
         this.userDao = userDao;
     }
 
+    /*
     public void upgradeLevels(){
         List<User> users = userDao.getAll();
         for(User user : users){
@@ -35,6 +36,31 @@ public class UserService {
             if(changed){userDao.update(user);}
 
         }
+    }
+*/
+    public void upgradeLevels(){
+        List<User> users = userDao.getAll();
+        for(User user : users){
+            if(canUpgradeLevel(user)){
+                upgradeLevel(user);
+            }
+        }
+    }
+
+    private boolean canUpgradeLevel(User user) {
+        Level currentLevel = user.getLevel();
+        switch(currentLevel) {
+            case BASIC: return (user.getLogin() >= 50);
+            case SILVER: return (user.getRecommend() >= 30);
+            case GOLD: return false;
+            default: throw new IllegalArgumentException("Unknown Level: " + currentLevel);
+        }
+    }
+
+    private void upgradeLevel(User user) {
+        if(user.getLevel() == Level.BASIC) user.setLevel(Level.SILVER);
+        else if(user.getLevel() == Level.SILVER) user.setLevel(Level.GOLD);
+        userDao.update(user);
     }
 
 
